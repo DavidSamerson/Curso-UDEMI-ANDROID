d16f6de6f1d50075f3d0a4ae82ef30d64f7b71ee
David Samerson <samerson10@gmail.com>
2017-11-30 15:23:20 -0300

    Criando módulos

--- APLICAÇÕES ANDROID IONIC 2 - UDEMI/cursoIonic2/src/app/app.module.ts
+++ APLICAÇÕES ANDROID IONIC 2 - UDEMI/cursoIonic2/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
+import { Box } from '../components/box/box';
 import { MinhaPagina } from './../pages/minha-pagina/minha-pagina';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -13,6 +14,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 @NgModule({
   declarations: [
+    Box,
     MinhaPagina,
     MyApp,
     HomePage,
---
+++ APLICAÇÕES ANDROID IONIC 2 - UDEMI/cursoIonic2/src/components/box/box.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { IonicModule } from 'ionic-angular';
+import { Box } from './box';
+
+@NgModule({
+  declarations: [
+    Box,
+  ],
+  imports: [
+    IonicModule,
+  ],
+  exports: [
+    Box
+  ]
+})
+export class BoxModule {}
