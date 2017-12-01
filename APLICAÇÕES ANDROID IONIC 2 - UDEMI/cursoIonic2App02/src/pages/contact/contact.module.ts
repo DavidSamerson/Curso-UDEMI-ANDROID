3bd3bf25ebec2693fc107a78f027d324dad90308
David Samerson <samerson10@gmail.com>
2017-12-01 14:31:46 -0300

    nova pagina contact

---
+++ APLICAÇÕES ANDROID IONIC 2 - UDEMI/cursoIonic2App02/src/pages/contact/contact.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { Contact } from './contact';
+
+@NgModule({
+  declarations: [
+    Contact,
+  ],
+  imports: [
+    IonicPageModule.forChild(Contact),
+  ],
+})
+export class ContactModule {}
---
+++ APLICAÇÕES ANDROID IONIC 2 - UDEMI/cursoIonic2App02/src/pages/contact/contact.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+$IMPORTSTATEMENT
+
+/**
+ * Generated class for the Contact page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+$IONICPAGE
+@Component({
+  selector: 'page-contact',
+  templateUrl: 'contact.html',
+})
+export class Contact {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad Contact');
+  }
+
+}
